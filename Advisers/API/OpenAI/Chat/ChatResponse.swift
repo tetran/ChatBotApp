c9c952cce79b45e4366438cb39a1fc9fea39c067
Koichi Kaneshige <coarse.ground@gmail.com>
2023-05-27 14:11:28 +0900

    レスポンスにusage追加

--- Advisers/API/OpenAI/Chat/ChatResponse.swift
+++ Advisers/API/OpenAI/Chat/ChatResponse.swift
@@ -16,16 +16,29 @@ struct ChatResponse: Codable {
     let created: Date
     
     let choices: [Choice]
-    
+
+    let usage: Usage
 }
 
 
 extension ChatResponse {
     struct Choice: Codable {
+        
         let index: Int
         
         let message: ChatMessage
         
         let finishReason: FinishReason?
+    }
+}
+
+extension ChatResponse {
+    struct Usage: Codable {
+        
+        let promptTokens: Int
+        
+        let completionTokens: Int
+        
+        let totalTokens: Int
     }
 }
